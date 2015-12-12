5cb5aa5f8ff69efcdebed44f946fc99fda705663
liam strevens <liamstrevens131@hotmail.com>
2015-12-12 15:12:34 +0000

    tries program

---
+++ Liam/trie loading/trie loading/IPrefixMatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trie_loading
+{
+    public interface IPrefixMatcher<V> where V : class
+    {
+        String GetPrefix();
+        void ResetMatch();
+        void BackMatch();
+        char LastMatch();
+        bool NextMatch(char next);
+        List<V> GetPrefixMatches();
+        bool IsExactMatch();
+        V GetExactMatch();
+    }
+}
